c6a1b4f8da0ab5bd6b1c070bb8e44a3f2ba12dd9
petit42 <petit@peccadilles.net>
2015-10-03 00:06:37 +0200

    [add] begin of script

--- check_update.sh
+++ check_update.sh
@@ -3,4 +3,26 @@
 # This script must be check if there're some update availables and send mail
 
 # Requirements 
-#  - apticron package
+#		- apticron package install and
+#			- change email contact with sed -i "s/EMAIL=\"root\"/EMAIL=\"admin@`cat /etc/yunohost/current_host`\"/" /etc/apticron/apticron.conf 
+#			- remove cron automatically installed with rm /etc/cron.d/apticron
+
+# Settings
+set -e
+set -o nounset
+
+########
+# MAIN #
+########
+
+# Apticron check (system and Yunohost)
+if test -x /usr/sbin/apticron
+	then 
+		sudo /usr/sbin/apticron --cron
+else 
+	echo "quoi retourner ?"
+fi
+
+
+
+
