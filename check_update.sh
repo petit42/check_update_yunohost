c88ed334d12427fd96032cc7cd6283ccfcc77540
petit42 <petit@peccadilles.net>
2015-10-03 00:37:48 +0200

    wip

--- check_update.sh
+++ check_update.sh
@@ -3,26 +3,17 @@
 # This script must be check if there're some update availables and send mail
 
 # Requirements 
-#		- apticron package install and
-#			- change email contact with sed -i "s/EMAIL=\"root\"/EMAIL=\"admin@`cat /etc/yunohost/current_host`\"/" /etc/apticron/apticron.conf 
-#			- remove cron automatically installed with rm /etc/cron.d/apticron
 
 # Settings
 set -e
 set -o nounset
 
+# Vars
+system_update="0"
+
 ########
 # MAIN #
 ########
+system_update=`apt-get --just-print upgrade 2>&1 | perl -ne 'if (/Inst\s([\w,\-,\d,\.,~,:,\+]+)\s\[([\w,\-,\d,\.,~,:,\+]+)\]\s\(([\w,\-,\d,\.,~,:,\+]+)\)? /i) {print "PROGRAM: $1 INSTALLED: $2 AVAILABLE: $3\n"}'`
 
-# Apticron check (system and Yunohost)
-if test -x /usr/sbin/apticron
-	then 
-		sudo /usr/sbin/apticron --cron
-else 
-	echo "quoi retourner ?"
-fi
-
-
-
-
+echo $system_update
