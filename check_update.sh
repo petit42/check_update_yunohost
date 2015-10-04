a127862e8fae33075816d7c1eddc275d8183479f
Petit <petit@peccadilles.net>
2015-10-04 14:20:45 +0200

    add cleaning

--- check_update.sh
+++ check_update.sh
@@ -5,8 +5,8 @@
 # Requirements 
 
 # Settings
-set -e
-set -o nounset
+#set -e
+#set -o nounset
 
 # Vars
 system_update="0"
@@ -19,7 +19,7 @@ mail_notification="petit@peccadilles.net"
 # MAIN #
 ########
 #sudo yunohost tools update 2>&1 > $dry_output
-update_system=`sed -n '/packages:/{:a;N;/apps:/!ba;p;}' $dry_output| sed "s/    name: //" | grep -vE "(^$|^ |packages:|apps:)"|cut -d " " -f1|sort -u` 
+#update_system=`sed -n '/packages:/{:a;N;/apps:/!ba;p;}' $dry_output| sed "s/    name: //" | grep -vE "(^$|^ |packages:|apps:)"|cut -d " " -f1|sort -u` 
 update_app=`sed -n '/apps:/,$p' $dry_output |grep id|sed "s/ *id: \(.*\)/\1/"`
 
 echo "From: labriqueinter.net" >$mail_content
@@ -28,13 +28,28 @@ echo "Subject: Updating LaBriqueInter.net" >>$mail_content
 echo "Mime-Version: 1.0" >>$mail_content
 echo "Content-Type: text/html; charset=\"iso-8859-15\"" >>$mail_content
 echo "Bonjour gros/se !" >>$mail_content
-echo "Ta BriqueInter.net a besoin d'amour…" >>$mail_content
-echo "Les paquets système suivant nécesssite d'être mis à jour:" >>$mail_content
-echo "$update_system" >>$mail_content
+echo -e "Ta BriqueInter.net a besoin d'amour !\n" >>$mail_content
 echo "" >>$mail_content
-echo "Les applications suivantes nécessite d'être mises à jour:" >>$mail_content
-echo "$update_app" >>$mail_content
+if [[ -n $update_system ]]
+then
+	echo "Le(s) paquet(s) système suivant nécesssite(nt) d'être mis à jour:" >>$mail_content
+	echo "$update_system" >>$mail_content
+	echo "" >>$mail_content
+fi
+if [[ -n $update_app ]]
+then  
+	echo "L(es) application(s) suivante(s) nécessite(nt) d'être mise(s) à jour:" >>$mail_content
+	echo "$update_app" >>$mail_content
+fi
 
 cat $mail_content | sendmail -t 
 
-# todo sup $dry_output $mail_content
+# clean 
+#if [[ -f $dry_output ]]
+#then 
+#	rm $dry_output
+#fi
+#if [[ -f $mail_content ]]
+#then
+#	rm $mail_content
+#fi
